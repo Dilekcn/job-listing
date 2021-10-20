9e6670e654ed7ef72f0d1c084bda824ff15f2d02
Dilekcn <dcanturk1@gmail.com>
2021-10-20 23:49:16 +0100

    fixed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 function Header() {
     return (
-        <div className="header_cont overflow-hidden ">
+        <div className="header_cont overflow-hidden sticky-top">
             <img src={'./images/bg-header-desktop.svg'} alt=""/>
             
         </div>
